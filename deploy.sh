c0493f52d10ad27658ef567455e0bde133aaac0c
bhushankhaire <bhushanpkhaire@gmail.com>
2020-08-11 13:27:27 +0530

    Create deploy.sh

---
+++ deploy.sh
@@ -0,0 +1,6 @@
+## Copyright © 2020, Oracle and/or its affiliates. 
+## All rights reserved. The Universal Permissive License (UPL), Version 1.0 as shown at http://oss.oracle.com/licenses/upl
+
+#!/bin/bash
+#sudo runuser -l opc -c "sudo docker login -u '<username>' -p '<ocir-token>' <region-prefix-name>"
+sudo runuser -l opc -c 'kubectl apply -f ../../deployment.yml'
